88b55229b5f5b0fcbf0b5e04b5ffab204c618317
aydaBastami <aydabstmi@gmail.com>
2021-07-29 12:10:22 -0700

    EveryThing

---
+++ src/components/welcome.js
@@ -0,0 +1,16 @@
+const Welcome = () => {
+return(
+  <h1> Welcome to react world </h1>  
+) 
+}
+
+const NameSite = () => {
+    return(
+        <div>
+<Welcome />
+<h2>Ayda Bastami</h2>
+        </div>
+    )
+}
+
+export default NameSite;
